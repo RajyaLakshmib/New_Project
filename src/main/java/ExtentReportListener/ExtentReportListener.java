3bc403c3a127830c8d03c35acaf7360a87bea72c
basuvula <rajyalakshmi777@gmail.com>
2018-12-17 15:45:10 +1100

    3rd commit

--- src/main/java/ExtentReportListener/ExtentReportListener.java
+++ src/main/java/ExtentReportListener/ExtentReportListener.java
@@ -8,6 +8,7 @@ import org.testng.xml.XmlSuite;
 
 public class ExtentReportListener implements IReporter {
 //Testing for CLone2
+	//testing for clone3
 	private ExtentReportListener extent;
 	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
 		// TODO Auto-generated method stub
